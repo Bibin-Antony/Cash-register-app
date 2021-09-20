049386bd8d864df328efc1df535b023c6297b38e
Bibin Antony <40894472+Bibin-Antony@users.noreply.github.com>
2021-09-20 17:40:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const billAmount = document.querySelector("#bill-amount");
-const cashGiven = document.querySelector("#cash-given")
-const checkButton = document.querySelector("#check-button");
-const errorMessage = document.querySelector("#error-message");
-const noOfNotes = document.querySelectorAll(".no-of-notes");
+var billAmount = document.querySelector("#bill-amount");
+var cashGiven = document.querySelector("#cash-given")
+var checkButton = document.querySelector("#check-button");
+var errorMessage = document.querySelector("#error-message");
+var noOfNotes = document.querySelectorAll(".no-of-notes");
 
 //converting to numbers
 const BillAmount = Number(billAmount.value);
@@ -10,9 +10,16 @@ const CashGiven = Number(cashGiven.value);
 
 const avaliableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
+
 checkButton.addEventListener("click", function validateBillAmount() {
-    hideMessage();
-    if (BillAmount > 0) {
+    
+
+const BillAmount = Number(billAmount.value);
+const CashGiven = Number(cashGiven.value);
+
+    
+    if (BillAmount > 0 && CashGiven > 0) {
+        hideMessage();
         if (CashGiven >= BillAmount) {
             const amountToBeReturned = CashGiven - BillAmount;
             calculateChange(amountToBeReturned); //passing vakue to calculate change
@@ -30,13 +37,13 @@ function hideMessage() {
 }
 
 function calculateChange(amountToBeReturned) {
-    for (let i = 0; i < avaliableNotes.length; i++) {
-        const numberofNotes = Math.trunc(amountToBeReturned / avaliableNotes[0]);
+    for (let i = 0; i < avaliableNotes.length; ++i) {
+        const numberofNotes = Math.trunc(amountToBeReturned / avaliableNotes[i]);
         amountToBeReturned %= avaliableNotes[i];
         noOfNotes[i].innerText = numberofNotes;
     }
 }
-console.log(BillAmount);
+
 
 function showMessage(message) {
     errorMessage.style.display = "block";
