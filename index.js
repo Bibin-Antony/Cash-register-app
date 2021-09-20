1705e79424fe6733b94263ff9b93b1867555ba35
Bibin Antony <40894472+Bibin-Antony@users.noreply.github.com>
2021-09-20 16:31:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const checkButton = document.querySelector("#check-button");
 const errorMessage = document.querySelector("#error-message");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
 
+//converting to numbers
+const BillAmount = Number(billAmount.value);
+const CashGiven = Number(cashGiven.value);
+
 const avaliableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
 checkButton.addEventListener("click", function validateBillAmount() {
     hideMessage();
-    if (billAmount.value > 0) {
-        if (cashGiven.value >= billAmount.value) {
-            const amountToBeReturned = cashGiven.value - billAmount.value;
+    if (BillAmount > 0) {
+        if (CashGiven >= BillAmount) {
+            const amountToBeReturned = CashGiven - BillAmount;
             calculateChange(amountToBeReturned); //passing vakue to calculate change
         } else {
             showMessage("The cash provided atlease be equal to the bill amount");
@@ -32,6 +36,7 @@ function calculateChange(amountToBeReturned) {
         noOfNotes[i].innerText = numberofNotes;
     }
 }
+console.log(BillAmount);
 
 function showMessage(message) {
     errorMessage.style.display = "block";
