5ff14f58b93a62b5224f8d6466e3c3b14e36b12a
Bibin Antony <40894472+Bibin-Antony@users.noreply.github.com>
2021-09-20 09:52:17 +0530

    update

---
+++ app.js
@@ -0,0 +1,19 @@
+var totalAmount = document.querySelector("#bill-amount");
+var cashGiven = document.querySelector("#cash-given");
+var check = document.querySelector("#check");
+var finalresult = document.querySelector("#finalresult1");
+
+
+
+check.addEventListener("click", () => {
+
+
+    console.log("actualAmount");
+});
+
+
+
+
+
+
+
